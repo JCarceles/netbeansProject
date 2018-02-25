a2b9ac3c6da454018a2f8d36f82828a01cda1d98
imac <imac@192.168.1.5>
2018-02-25 19:33:44 +0100

    segunda modificación del proyecto desde nb

---
+++ Githubdemo/src/java/bean/Producto.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author imac
+ */
+public class Producto {
+    private String idproducto;
+    private int cantidad;
+
+    public String getIdproducto() {
+        return idproducto;
+    }
+
+    public void setIdproducto(String idproducto) {
+        this.idproducto = idproducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+    
+}
